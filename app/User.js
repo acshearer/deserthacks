6c253f9b14d8bf44d1045cf248e37bb959f25ffc
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2017-02-26 02:24:59 -0700

    Update user schema to include more info on revents

--- app/User.js
+++ app/User.js
@@ -1,24 +1,28 @@
 var mongoose = require('mongoose');
 
 var schema = mongoose.Schema({
-		user : {
-			google : {
-				id : String,
-				token : String,
-				email : String,
-				name : String,
-			},
-			data : {
-				userTags : [String],
-				friends : [String],
-				visibilityModifier : Number,
-				schedule : [{scheduleEvent: {ignore : Boolean, 
-											start_date : Date,
-											end_date : Date,
-											days_of_week: String,
-											start_time : Date,
-											end_time : Date}}],
-			}
-	}});
+        user : {
+                google : {
+                        id : String,
+                        token : String,
+                        email : String,
+                        name : String,
+                },
+                data : {
+                        userTags : [String],
+                        friends : [String],
+                        visibilityModifier : Number,
+                        schedule : [
+                                {scheduleEvent: {
+                                        name : String,
+                                        location : String,
+                                        ignore : Boolean, 
+                                        start_date : Date,
+                                        end_date : Date,
+                                        days_of_week: String,
+                                        start_time : Date,
+                                        end_time : Date}}],
+                }
+        }});
 
 module.exports = mongoose.model('User', schema);
