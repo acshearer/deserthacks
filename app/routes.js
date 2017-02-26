2102b1d747b37fea1654004a835c5552974ed9e9
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2017-02-26 08:18:51 -0700

    Factor out free friends code

--- app/routes.js
+++ app/routes.js
@@ -156,13 +156,7 @@ module.exports = function(app, passport){
         app.get('/findFreeFriends', function(req, res) {
                 var user = req.user;
 
-                var friends = user.user.data.friends;
-                var now = new Date();
-
-                var freeFriends = friends.filter(friendId => {
-                        const friendDoc = getDocumentFromId(friendId);
-                        return isFree(friendDoc, now);
-                });
+                freeFriends = findFreeFriends(user);
 
                 res.setHeader('Content-Type', 'application/json');
                 res.send(JSON.stringify({freeFriends: freeFriends}));
@@ -226,16 +220,56 @@ module.exports = function(app, passport){
         
 
         app.post('/alexa', function(req, res) {
+                var body = req.body;
+                var intent = req.request.intent.name;
+
+                var response = "";
+
+                switch(intent) {
+                        case "FreeFriends": {
+                                let freeFriends = findFreeFriends(req.user);
+                                let names = freeFriends.map(friendId => {
+                                        const friendDoc = getDocumentFromId(friendId);
+                                        return friendDoc.user.google.name;
+                                });
+
+                                nameList = englishConcat(names);
+
+                                if (names.length == 0) {
+                                        response = "You have no free friends right now. Perhaps you should make some?";
+                                } else if (names.length == 1) {
+                                        response = "Your only free friend right now is " + names[0];
+                                } else {
+                                        response = "You have " + names.length + " free friends right now. ";
+                                        response += "They are " + namelist + ".";
+
+                                }
+                        }
+                }
+
+
                 res.json({
                         "version": "1.0",
                         "response": {
                                 "shouldEndSession": true,
                                 "outputSpeech": {
                                         "type": "SSML",
-                                        "ssml": "<speak>Test</speak>"
+                                        "ssml": "<speak>" + response + "</speak>"
                                 }}
                 });
         });
+}
+
+function englishConcat(list) {
+        if (list.length == 0) {
+                return "";
+        } else if (list.length == 1) {
+                return list[0];
+        } else if (list.length == 2) {
+                return list[1] + " and " + list[2];
+        } else {
+                return list.slice(0, -1).join(", ") + ", or " + list[list.length-1];
+        }
 }
 
 // userDoc is a user document
@@ -265,6 +299,18 @@ function isFree(userDoc, time) {
 
         });
 
+}
+
+function findFreeFriends(userDoc) {
+        var friends = user.user.data.friends;
+        var now = new Date();
+
+        var freeFriends = friends.filter(friendId => {
+                const friendDoc = getDocumentFromId(friendId);
+                return isFree(friendDoc, now);
+        });
+
+        return freeFriends;
 }
 
 function getDocumentFromId(id){
